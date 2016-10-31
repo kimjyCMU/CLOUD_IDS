d240ab7a3e1c0c4549583c2d53d0d7957a237458
JY <kimjy@andrew.cmu.edu>
2016-10-31 17:22:32 -0400

    remove deleted files

--- clientAgent/deploy/scripts/stop.sh
+++ clientAgent/deploy/scripts/stop.sh
@@ -8,7 +8,6 @@ IPlist="$DIR/deploy/scripts/iplist.txt"
 IPs=$(cat $IPlist) 
 File="cloudSecurity.jar"
 
-#certification established - need to install 'expect' 
 for i in $IPs  
 do   
 echo "** Stop : "$i
